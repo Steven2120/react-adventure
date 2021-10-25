aefbfc1e9f8322893c9c32b343ce63cbf618dac5
Steven Moyano <stevenmoyano1@gmail.com>
2021-10-25 17:23:47 -0500

    added services, products, sign-up pages as well as completed card layout

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
+import Products from "./components/Products/Products";
+import Services from "./components/Services/Services";
+import SignUp from "./components/Sign-up/SignUp";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./MainRouter.css";
 
@@ -11,6 +14,9 @@ function MainRouter() {
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/products" component={Products} />
+          <Route path="/services" component={Services} />
+          <Route path="/sign-up" component={SignUp} />
         </Switch>
       </Router>
     </>
--- src/components/CardItems/Cards.js
+++ src/components/CardItems/Cards.js
@@ -17,9 +17,29 @@ function Cards() {
             />
             <CardItems
               src="../../images/img-2.jpg"
-              text="Explore the hidden waterfall deep inside the Amazon"
-              label="Adventure"
+              text="Travel through the islands of Bali"
+              label="Luxury"
               path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItems
+              src="../../images/img-3.jpg"
+              text="Set sail in the Atlantic Ocean visiting uncharted waters"
+              label="Mystery"
+              path="/services"
+            />
+            <CardItems
+              src="../../images/img-4.jpg"
+              text="Experience Football on top of the Himalayan mountains"
+              label="Adventure"
+              path="/products"
+            />
+            <CardItems
+              src="../../images/img-8.jpg"
+              text="Ride through the Sahara desert on a guided camel tour"
+              label="Adrenaline"
+              path="/sign-up"
             />
           </ul>
         </div>
